01962eb323275cd5edf1b5e5b72407bd8cc23dec
Noah Gift <noah.gift@gmail.com>
2022-05-27 22:43:55 +0000

    adding summary

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from transformers import pipeline
 import gradio as gr
 
-model = pipeline("summarize")
+model = pipeline("summarization")
 
 def predict(prompt):
     summary = model(prompt)[0]["summarize_text"]
     return summary
 
-gr.Interface(fn=predict, inputs="text", outputs="text").launch()
+gr.Interface(fn=predict, inputs="text to summarize", outputs="summary output").launch()
